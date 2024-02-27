6aaf2d2d3ac566b95d5b02c086bd31b28848e6d1
Anis Moubarik <anis.moubarik@solita.fi>
2024-02-27 19:09:23 +0200

    chart should work now

--- bookcollectionback/db/database.js
+++ bookcollectionback/db/database.js
@@ -1,5 +1,6 @@
 const sqlite3 = require('sqlite3').verbose();
 const logger = require('../logger');
+const fs = require('fs');
 
 const SOURCE = process.env.DB_URL || "./db/books_23.db";
 
@@ -7,6 +8,8 @@ function createDbConnection() {
     let db = new sqlite3.Database(SOURCE, sqlite3.OPEN_READWRITE, (err) => {
         if (err) {
             logger.error(err.message);
+            logger.error("Source used: " + SOURCE);
+            logger.error("DB file exists: " + fs.existsSync(SOURCE));
             throw err;
         } else {
             logger.info('Connected to db');
